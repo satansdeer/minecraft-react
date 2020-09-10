903e40fe5d2794d3af6f0046a3d3c81f501aef89
chris <parweb@gmail.com>
2020-09-10 14:52:26 +0200

    recoil implementation

--- src/PointerLockControls.js
+++ src/PointerLockControls.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react'
-import { PointerLockControls as PointerLockControlsImpl } from "three/examples/jsm/controls/PointerLockControls"
-import { useThree, extend } from 'react-three-fiber'
-import { useRef } from 'react'
+import React, { useEffect } from 'react';
+import { PointerLockControls as PointerLockControlsImpl } from 'three/examples/jsm/controls/PointerLockControls';
+import { useThree, extend } from 'react-three-fiber';
+import { useRef } from 'react';
 
-extend({ PointerLockControlsImpl })
+extend({ PointerLockControlsImpl });
 
-export const PointerLockControls = (props) => {
-  const { camera, gl } = useThree()
-  const controls = useRef()
+export const PointerLockControls = props => {
+  const { camera, gl } = useThree();
+  const controls = useRef();
 
   useEffect(() => {
-    document.addEventListener("click", () => {
-      controls.current.lock()
-    })
-  }, [])
+    document.addEventListener('click', () => {
+      controls.current.lock();
+    });
+  }, []);
 
   return (
-    <pointerLockControlsImpl 
+    <pointerLockControlsImpl
       ref={controls}
       args={[camera, gl.domElement]}
       {...props}
     />
-  )
-}
+  );
+};
